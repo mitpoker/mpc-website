e8fcef47108edfa7895bca0214949c41bd0bf45f
Robert Cunningham <robertvcunningham@gmail.com>
2023-04-24 15:37:04 -0400

    Added alumni to membersts

---
+++ src/data/members.ts
@@ -0,0 +1,269 @@
+// Dynamic await import() or require() are not supported. The import must be static so it can be analyzed at build time.
+
+import AllenChengImage from "Allen_Cheng_Profile_Picture.jpg"
+import AllenDingImage from "Allen_Ding_Profile_Picture.png"
+import AllenWangImage from "Allen_Wang_Profile_Picture.png"
+import AndrewDiazImage from "Andrew_Diaz_Profile_Picture.jpg"
+import AnnieChenImage from "Annie_Chen_Profile_Picture.jpg"
+import AnthonyNardomarinoImage from "Anthony_Nardomarino_Profile_Picture.jpeg"
+import CarlWeiImage from "Carl_Wei_Profile_Picture.jpg"
+import ChristianCamachoImage from "Christian_Camacho_Profile_Picture.jpg"
+import DanielSunImage from "Daniel_Sun_Profile_Picture.png"
+import DannyYuanImage from "Danny_Yuan_Profile_Picture.jpg"
+import DavidZhengImage from "David_Zheng_Profile_Picture.jpg"
+import DavidWuImage from "David_Wu_Profile_Picture.png"
+import DionLowImage from "Dion_Low_Profile_Picture.jpg"
+import EmilyXieImage from "Emily_Xie_Profile_Picture.jpg"
+import EricQianImage from "Eric_Qian_Profile_Picture.jpg"
+import EricWangImage from "Eric_Wang_Profile_Picture.jpeg"
+import HannaChenImage from "Hanna_Chen_Profile_Picture.png"
+import HenryHeibergerImage from "Henry_Heiberger_Profile_Picture.png"
+import IsaacZhangImage from "Isaac_Zhang_Profile_Picture.jpg"
+import JeffreyLuImage from "Jeffrey_Lu_Profile_Picture.jpg"
+import JenniferWangImage from "Jennifer_Wang_Profile_Picture.jpg"
+import JoanneLeeImage from "Joanne_Lee_Profile_Picture.jpg"
+import JoshuaNwakobyImage from "Joshua_Nwakoby_Profile_Picture.jpg"
+import JustinShanImage from "Justin_Shan_Profile_Picture.jpg"
+import KarenGaoImage from "Karen_Gao_Profile_Picture.jpg"
+import KevinJiangImage from "Kevin_Jiang_Profile_Picture.jpg"
+import KevinLiuImage from "Kevin_Liu_Profile_Picture.jpg"
+import KevinWenImage from "Kevin_Wen_Profile_Picture.jpg"
+import KevinZhengImage from "Kevin_Zheng_Profile_Picture.png"
+import LaurenLiImage from "Lauren_Li_Profile_Picture.jpg"
+import LinhNguyenImage from "Linh_Nguyen_Profile_Picture.jpg"
+import MartinMaImage from "Martin_Ma_Profile_Picture.jpg"
+import MeghaMaranImage from "Megha_Maran_Profile_Picture.jpg"
+import MichaelZhaoImage from "Michael_Zhao_Profile_Picture.jpg"
+import MichelleLiImage from "Michelle_Li_Profile_Picture.png"
+import MideumParkImage from "Mideum_Park_Profile_Picture.jpg"
+import NaailLakhaniImage from "Naail_Lakhani_Profile_Picture.jpg"
+import NathanChenImage from "Nathan_Chen_Profile_Picture.jpg"
+import PawanGoyalImage from "Pawan_Goyal_Profile_Picture.jpg"
+import PennyTanImage from "Penny_Tan_Profile_Picture.jpeg"
+import RickHuangImage from "Rick_Huang_Profile_Picture.jpg"
+import RobertCunninghamImage from "Robert_Cunningham_Profile_Picture.png"
+import RyanKimImage from "Ryan_Kim_Profile_Picture_JWZiMik.jpg"
+import SamirAminImage from "Samir_Amin_Profile_Picture.jpg"
+import SarahWangImage from "Sarah_Wang_Profile_Picture.jpg"
+import ThomasGuoImage from "Thomas_Guo_Profile_Picture.jpg"
+import TianchengQinImage from "Tiancheng_Qin_Profile_Picture.jpg"
+import TristanCulpImage from "Tristan_Culp_Profile_Picture.png"
+import TristanShinImage from "Tristan_Shin_Profile_Picture.jpg"
+import VarianZhang from "Image Varian_Zhang _Profile_Picture.jpeg"
+import VickyChenImage from "Vicky_Chen_Profile_Picture.jpg"
+import WendySunImage from "Wendy_Sun_Profile_Picture.jpg"
+import WendyYinImage from "Wendy_Yin_Profile_Picture.jpg"
+import WilliamCuozzoImage from "William_Cuozzo_Profile_Picture.jpg"
+import { StaticImageData } from "next/image"
+
+export const members: Array<Member> = [
+  {
+    name: "Danny Yuan",
+    username: "dyn_mpc",
+    year: 2015,
+    bio: "Danny founded the club with his friends in 2012. He graduated MIT with a Bachelor of Science degree in Computer Science and in Mathematics, and a Masters of Engineering degree in Computer Science with a concentration in Machine Learning. He was an active member of the Poker Society when he studied abroad at the University of Cambridge and a three-time finalist for the MIT Pokerbots Programming Competition. He has played in the WSOP Main Event once and wants to win a bracelet in the future. Previously, he has worked at Goldman Sachs, Google, and Optiver. He is currently founder and CEO of 8Blocks Capital.",
+    image: DannyYuanImage,
+    alum: true,
+  },
+  {
+    name: "Martin Ma",
+    username: "DMa_MIT",
+    year: 2016,
+    bio: "Martin is a senior majoring in Computer Science and minoring in Management Science and Economics. He is interested in the trading industry and did an internship at Susquehanna International Group (SIG), learning about equity options and playing a lot of poker. He has also interned at Altimeter Capital Management as a hedge fund summer analyst and Morgan Stanley as a fixed income strats summer analyst. In his free time, he enjoys watching and playing sports, Pokemon, and hanging out with his frat, Pi Lambda Phi.",
+    image: MartinMaImage,
+    alum: true,
+  },
+  {
+    name: "Kevin Wen",
+    username: "KevinWen_MIT",
+    year: 2016,
+    bio: "Kevin graduated in 2016 with a major in computer science. At MIT, Kevin also served as the president and organizer of MIT Pokerbots starting back in 2014. He interned at SIG in 2015 before joining them full time as an index trader in 2017.",
+    image: KevinWenImage,
+    alum: true,
+  },
+  {
+    name: "Dion Low",
+    username: "DionLow_MIT",
+    year: 2016,
+    bio: "Dion is a senior majoring in Computer Science. He is one of the MIT Poker Club founding members back in 2012 with just 5 people. On campus, he is also co-presidents for the Korean Student Association and he has dabbled in robotics and machine vision during IAP Hong Kong. During his summers he has traveled over Southeast Asia while interning for start-up environments in Singapore as well as taking a position in cyber security at Accenture. Dion enjoys scuba diving, swimming, deep tissue massage, photography, dancing, and hanging out with his bros at Phi Beta Epsilon",
+    image: DionLowImage,
+    alum: true,
+  },
+  {
+    name: "David Zheng",
+    username: "dzheng_MIT",
+    year: 2018,
+    bio: "David graduated in 2018 with a major in computer science and an MEng in artificial intelligence under Prof. Joshua B. Tenenbaum. Since then, David has been working as an algo developer at Hudson River Trading.",
+    image: DavidZhengImage,
+    alum: true,
+  },
+  {
+    name: "Kevin Liu",
+    username: "UilNivek",
+    year: 2018,
+    bio: "Kevin graduated with a B.S. in Mathematics and Computer Science in 2018 and an M.Eng. in Computer Engineering in 2019. He now works as a trader at Five Rings Capital in New York City alongside Jeffrey. In his free time, he enjoys reading, hiking, and playing tennis. Kevin also enjoys playing online high-stakes play-money poker. He hopes to participate in the WSOP someday.",
+    image: KevinLiuImage,
+    alum: true,
+  },
+  {
+    name: "Annie Chen",
+    username: "achen_MIT",
+    year: 2018,
+    bio: "Annie served as one of MPC's 2016-2017 Co-Presidents. She graduated with an Economics major and a Management minor. She is currently an Associate Consultant at Semler Brossy Consulting Group in NYC. Previously, she has TA’d 15.665 and interned at Booz Allen Hamilton. She enjoys traveling, running, tennis, hiking, cooking, and hanging out with friends or puppies.",
+    image: AnnieChenImage,
+    alum: true,
+  },
+  {
+    name: "Linh Nguyen",
+    username: "lbnguy",
+    year: 2018,
+    bio: "Linh graduated with a major in Chemical Engineering and a minor in Music. She was involved in AIChE and Energy Club and also impulsively joined Dance Troupe and the Senegalese Drumming Ensemble her senior year. Linh is currently living in the SF Bay Area and working for Chevron on process development of petroleum additives. In her free time, she likes to compose and arrange jazz music, play tennis, and brew her own beer. She also loves to travel and ran away to Europe for 2 months after graduating!",
+    image: LinhNguyenImage,
+    alum: true,
+  },
+  {
+    name: "Jeffrey Lu",
+    username: "JLu_MIT",
+    year: 2018,
+    bio: "Jeffrey graduated with a double major in Computer Science and Mathematics. He is currently working full-time as a software developer at Five Rings Capital in New York City (with fellow alum Kevin Liu!). In his free time, he enjoys hanging out with friends, whether that entails playing board games or traveling to exotic places (Brooklyn) to get dim sum. He also enjoys swimming and cooking on a regular basis.",
+    image: JeffreyLuImage,
+    alum: true,
+  },
+  {
+    name: "Joanne Lee",
+    username: "leekkww",
+    year: 2019,
+    bio: "Joanne graduated in 2019 with a degree in 6-3. Prior to graduating, she interned at Google, Jane Street, and Broadway Technologies, but she is now working full time in San Francisco as a software engineer at Plaid Inc. In her free time, she likes playing poker (duh), rock climbing, cooking and traveling. Feel free to find her if you're ever in the Bay area!",
+    image: JoanneLeeImage,
+    alum: true,
+  },
+  {
+    name: "Rick Huang",
+    username: "ShiningSnow",
+    year: 2019,
+    bio: "Rick is a recent graduate who majored in Computer Science and minored in Math. Currently, he lives in the Bay Area, working as an engineer at Nuro. Other than continuing to play poker, he enjoys lifting, playing board games, and recently started getting into martial arts.",
+    image: RickHuangImage,
+    alum: true,
+  },
+  {
+    name: "Tiancheng Qin",
+    username: "Ectospirit",
+    year: 2020,
+    bio: "TC graduated in 2020 with a double major in Math and Computer Science, and is now working full time at Jane Street. Aside from poker, TC enjoys beatboxing, rock climbing, and playing all sorts of card games and puzzles!",
+    image: TianchengQinImage,
+    alum: true,
+  },
+  {
+    name: "Allen Cheng",
+    username: "hermitquack",
+    year: 2020,
+    bio: "Allen graduated in 2020 with a BS in 8 and 14-2 and a minor in IDS. He currently lives in New York City, working as a researcher at Two Sigma. Besides poker, Allen enjoys fitness, chess, and football analysis. Catch him in the city sometime!",
+    image: AllenChengImage,
+    alum: true,
+  },
+  {
+    name: "Eric Qian",
+    username: "discoversaur",
+    year: 2021,
+    bio: "Eric is a senior majoring in Computer Science. In his free time, Eric enjoys reading, playing volleyball, and travelling with his friends. Recently, he’s picked up computer vision research and learning history. He hopes to start his own tech company one day in the future.",
+    image: EricQianImage,
+    alum: true,
+  },
+  {
+    name: "Sarah Wang",
+    username: "swangers",
+    year: 2021,
+    bio: "Sarah is a senior majoring in Math and Computer Science and minoring in Economics. Other than poker, she enjoys eating, food, hiking, the beach, board games, and travelling the world! She spent this past summer in NYC, and she lives in Seattle!",
+    image: SarahWangImage,
+    alum: true,
+  },
+  {
+    name: "Emily Xie",
+    username: "emxie",
+    year: 2021,
+    bio: "Emily is a senior, double majoring in Physics and Math with a HASS Concentration in Music. Aside from poker, Emily enjoys reading, singing, ping pong, watching movies, and all sorts of card games and puzzles!",
+    image: EmilyXieImage,
+    alum: true,
+  },
+  {
+    name: "Michael Zhao",
+    username: "michaelczhao",
+    year: 2021,
+    bio: "Michael is a senior from New Jersey majoring in Economics with minors in Finance and Literature. Outside of poker, he enjoys playing tennis, hiking, and playing strategy games like Settlers of Catan. He will be working in private equity in NYC after graduating.",
+    image: MichaelZhaoImage,
+    alum: true,
+  },
+  {
+    name: "Wendy Yin",
+    username: "wendyqq2000",
+    year: 2022,
+    bio: "Wendy is a senior majoring in Computer Science and Economics. She is also a member of Asian Dance Team, Ridonkulous, DanceTroupe, and Pi Phi. Aside from poker, she enjoys dancing, playing piano, reading, and journalling.",
+    image: WendyYinImage,
+    alum: true,
+  },
+  {
+    name: "Daniel Sun",
+    username: "SunnyD11235",
+    year: 2022,
+    bio: "Daniel is a junior majoring in Computer Science and Mathematics. Outside of poker, Daniel enjoys playing Magic the Gathering, rock climbing, and urban exploring. In the future, Daniel plans to either go into AI research or work at a startup.",
+    image: DanielSunImage,
+    alum: true,
+  },
+  {
+    name: "Anthony Nardomarino",
+    username: "anardomarino",
+    year: 2022,
+    bio: "Anthony is a junior majoring in EECS with a focus in Neuroengineering. Besides Poker, he loves listening to music from the 60s, trying to play any stringed instrument you can find, and telling people about how horseracing is an untapped market.",
+    image: AnthonyNardomarinoImage,
+    alum: true,
+  },
+  {
+    name: "David Wu",
+    username: "crastybow",
+    year: 2022,
+    bio: "David is a junior majoring in Math and Computer Science (in that order). When he's not busy saving the world with math, he's a lover of dance, sci-fi, and good food. He also enjoys curating seasonal playlists on his Spotify; his favorite artists include Overwerk and Vicetone",
+    image: DavidWuImage,
+    alum: true,
+  },
+  {
+    name: "William Cuozzo",
+    username: "wcuozzo",
+    year: 2022,
+    bio: "Will is a junior majoring in Physics and Computer Science. He is also a member of the Chess Club, Machine Intelligence Community, and Zeta Psi. Aside from poker, he enjoys running and playing pickup basketball.",
+    image: WilliamCuozzoImage,
+    alum: true,
+  },
+  {
+    name: "Kevin Jiang",
+    username: "tiltpoker07",
+    year: 2022,
+    bio: "Kevin is a junior majoring in Computer Science. In addition to his love for poker, he enjoys Taekwondo, meditation, and entrepreneurship.",
+    image: KevinJiangImage,
+    alum: true,
+  },
+  {
+    name: "Carl Wei",
+    username: "carllman108",
+    year: 2023,
+    bio: "Carl is an exchange student from London School of Economics majoring in Statistics. He's a junior by American standards. Outside of poker, he likes watching films, playing ping pong and tennis, and enjoying nice food with friends.",
+    image: CarlWeiImage,
+    alum: true,
+  },
+  {
+    name: "Ryan Kim",
+    username: "ryank347",
+    year: 2023,
+    bio: "Ryan is a sophomore studying CS and Math. Besides poker, he likes singing, playing guitar, watching American Ninja Warrior, and doing spontaneous things with friends!",
+    image: RyanKimImage,
+    alum: true,
+  },
+]
+
+export type Member = {
+  name: string
+  username: string
+  year: number
+  bio: string
+  image: StaticImageData
+  alum: boolean
+}
