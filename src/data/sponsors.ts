deb6a1d4f9d331fdf14f0d15f0f787baada86c6f
Robert Cunningham <robertvcunningham@gmail.com>
2023-04-24 19:13:24 -0400

    add links to sponsors

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -21,6 +21,7 @@ export type Sponsor = {
   tier: SponsorTier
   image: StaticImageData
   text_md?: string
+  link: string
 }
 
 export const SponsorData: Array<Sponsor> = [
@@ -28,6 +29,7 @@ export const SponsorData: Array<Sponsor> = [
     name: "Sig",
     tier: SponsorTier.Platinum,
     image: SigImage,
+    link: "https://sig.com",
     text_md:
       "SIG is a global quantitative trading firm founded with an entrepreneurial mindset and rigorous analytical approach to decision making. SIG also values gaming, poker especially, as a way to teach analytical skills used in trading - check out the [Gamer Blog](https://www.raiseyourgame.com/)!",
   },
@@ -35,6 +37,7 @@ export const SponsorData: Array<Sponsor> = [
     name: "Citadel",
     tier: SponsorTier.Gold,
     image: CitadelImage,
+    link: "https://citadel.com",
     text_md:
       "Citadel is a leading investor in the world's financial markets. For over a quarter of a century, Citadel has sought to deliver market-leading investment returns to capital partners. Citadel Securities aims to create the fairest, most transparent, and open markets in the world.",
   },
@@ -42,6 +45,7 @@ export const SponsorData: Array<Sponsor> = [
     name: "Two Sigma",
     tier: SponsorTier.Gold,
     image: TwoSigmaImage,
+    link: "https://twosigma.com",
     text_md:
       "At Two Sigma, we use data science and technology to discover value in markets worldwide. Our community of scientists, technologists and academics collaborate to solve some of the most challenging economic problems. We don’t just wonder— we create systematic tools and technologies to forecast the future of global markets.",
   },
@@ -49,6 +53,7 @@ export const SponsorData: Array<Sponsor> = [
     name: "Jane Street",
     tier: SponsorTier.Gold,
     image: JaneStreetImage,
+    link: "https://janestreet.com",
     text_md:
       "Jane Street is a quantitative trading firm that uses innovative technology, a scientific approach and a deep understanding of markets to guide our business. The markets change rapidly, and we need to change faster still. Every day, we come to work with new problems to solve, new systems to build and new theories to test. We’re always looking for people to join us and help come up with that next great idea.",
   },
@@ -56,25 +61,30 @@ export const SponsorData: Array<Sponsor> = [
     name: "IMC",
     tier: SponsorTier.Silver,
     image: IMCImage,
+    link: "https://www.imc.com/us/",
   },
   {
     name: "Five Rings",
     tier: SponsorTier.Silver,
     image: FiveRingsImage,
+    link: "https://www.fiverings.com/",
   },
   {
     name: "Flow Traders",
     tier: SponsorTier.Bronze,
     image: FlowImage,
+    link: "https://www.flowtraders.com/",
   },
   {
     name: "Cubist",
     tier: SponsorTier.Bronze,
     image: CubistImage,
+    link: "https://www.point72.com/cubist/",
   },
   {
     name: "Vatic",
     tier: SponsorTier.Bronze,
     image: VaticImage,
+    link: "https://vaticlabs.ai",
   },
 ]
--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -2,6 +2,7 @@ import ReactMarkdown from 'react-markdown'
 import { Sponsor, SponsorData, SponsorTier } from '@/data/sponsors'
 import Image from "next/image"
 import _ from 'lodash'
+import Link from 'next/link'
 
 const tiers = Object.values(SponsorTier).filter(value => typeof value === 'string') as SponsorTier[]
 
@@ -15,7 +16,7 @@ const tierConfig: Record<SponsorTier, { width: string, text: boolean }> = {
 const Sponsors = () => {
 	return <div className="max-w-prose mx-auto">
 		<div className="px-2 flex flex-col py-3">
-			<h2 className="text-zinc-700 font-bold text-2xl mx-auto my-5 text-center">Sponsors</h2>
+			{/* <h2 className="text-zinc-700 font-bold text-2xl mx-auto my-5 text-center">Sponsors</h2> */}
 			<div className="prose prose-zinc">
 				{tiers.map((t, i) => (
 					<SponsorsAtTier key={i} tier={t} sponsors={SponsorData.filter(s => s.tier == t)}></SponsorsAtTier>
@@ -32,7 +33,9 @@ const SponsorsAtTier = ({ tier, sponsors }: { tier: SponsorTier, sponsors: Array
 		<div className={text ? "" : `grid gap-5 ${imageWidth} items-center` /*"flex flex-row items-center"*/}>{
 			_.shuffle(sponsors).map((s, i) => (
 				<div key={i} className="">
-					<Image {...s.image} alt={s.name} className={" mx-auto"}></Image>
+					<Link href={s.link} target="_blank">
+						<Image {...s.image} alt={s.name} className={" mx-auto"}></Image>
+					</Link>
 					<ReactMarkdown>
 						{s.text_md!}
 					</ReactMarkdown>
