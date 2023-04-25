b2e04086c059b48638bbe0612c175c25a5bdb498
Robert Cunningham <robertvcunningham@gmail.com>
2023-04-24 20:50:20 -0400

    add events calendar

---
+++ src/pages/events.tsx
@@ -0,0 +1,10 @@
+const Events = () => {
+	return (
+		<div className="w-full flex flex-col items-center">
+			<h2 className="text-zinc-700 font-bold text-2xl mx-auto my-5 text-center">Upcoming Events</h2>
+			<iframe src="https://calendar.google.com/calendar/embed?src=mitpokerexec%40gmail.com&ctz=America%2FNew_York" style={{ border: 0 }} width="800" height="600"></iframe>
+		</div>
+	)
+}
+
+export default Events
