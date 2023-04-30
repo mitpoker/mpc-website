ca59f6fe588bccb24cc8f454484f9b96e95eedaf
Eric Wang <ewang1@mit.edu>
2023-04-30 19:09:29 -0400

    updated sponsors

--- src/lib/sponsors.ts
+++ src/lib/sponsors.ts
@@ -1,12 +1,9 @@
 import { StaticImageData } from "next/image"
 import SigImage from "../../images/sponsors/sig.jpg"
 import CitadelImage from "../../images/sponsors/citadel.png"
-import TwoSigmaImage from "../../images/sponsors/twosigma.jpg"
-import CubistImage from "../../images/sponsors/cubist.png"
-import FiveRingsImage from "../../images/sponsors/fiverings.png"
-import FlowImage from "../../images/sponsors/flow.png"
-import IMCImage from "../../images/sponsors/imc.png"
 import JaneStreetImage from "../../images/sponsors/janestreet.png"
+import JumpImage from "../../images/sponsors/jump.png"
+import TwoSigmaImage from "../../images/sponsors/twosigma.jpg"
 import VaticImage from "../../images/sponsors/vatic.png"
 
 export enum SponsorTier {
@@ -42,14 +39,6 @@ export const SponsorData: Array<Sponsor> = [
       "Citadel is a leading investor in the world's financial markets. For over a quarter of a century, Citadel has sought to deliver market-leading investment returns to capital partners. Citadel Securities aims to create the fairest, most transparent, and open markets in the world.",
   },
   {
-    name: "Two Sigma",
-    tier: SponsorTier.Gold,
-    image: TwoSigmaImage,
-    link: "https://twosigma.com",
-    text_md:
-      "At Two Sigma, we use data science and technology to discover value in markets worldwide. Our community of scientists, technologists and academics collaborate to solve some of the most challenging economic problems. We don’t just wonder— we create systematic tools and technologies to forecast the future of global markets.",
-  },
-  {
     name: "Jane Street",
     tier: SponsorTier.Gold,
     image: JaneStreetImage,
@@ -58,33 +47,27 @@ export const SponsorData: Array<Sponsor> = [
       "Jane Street is a quantitative trading firm that uses innovative technology, a scientific approach and a deep understanding of markets to guide our business. The markets change rapidly, and we need to change faster still. Every day, we come to work with new problems to solve, new systems to build and new theories to test. We’re always looking for people to join us and help come up with that next great idea.",
   },
   {
-    name: "IMC",
+    name: "Jump",
     tier: SponsorTier.Silver,
-    image: IMCImage,
-    link: "https://www.imc.com/us/",
+    image: JumpImage,
+    link: "https://jumptrading.com",
+    text_md:
+      "Jump Trading is committed to world class research. We empower exceptional talents in Mathematics, Physics, and Computer Science to seek scientific boundaries, push through them, and apply cutting-edge research to global financial markets.",
   },
   {
-    name: "Five Rings",
+    name: "Two Sigma",
     tier: SponsorTier.Silver,
-    image: FiveRingsImage,
-    link: "https://www.fiverings.com/",
-  },
-  {
-    name: "Flow Traders",
-    tier: SponsorTier.Bronze,
-    image: FlowImage,
-    link: "https://www.flowtraders.com/",
-  },
-  {
-    name: "Cubist",
-    tier: SponsorTier.Bronze,
-    image: CubistImage,
-    link: "https://www.point72.com/cubist/",
+    image: TwoSigmaImage,
+    link: "https://twosigma.com",
+    text_md:
+      "At Two Sigma, we use data science and technology to discover value in markets worldwide. Our community of scientists, technologists and academics collaborate to solve some of the most challenging economic problems. We don’t just wonder— we create systematic tools and technologies to forecast the future of global markets.",
   },
   {
     name: "Vatic",
     tier: SponsorTier.Bronze,
     image: VaticImage,
     link: "https://vaticlabs.ai",
+    text_md:
+        "Vatic Investments is a global liquidity provider, where traders, AI researchers, and technologists collaborate to develop autonomous trading agents and cutting edge technology. We work together, building systems that boost market efficiency and transparency.",
   },
 ]
