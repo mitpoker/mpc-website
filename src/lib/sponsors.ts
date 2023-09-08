423d6346c48b66a41ee160900020f8a7f660d88e
Eric Wang <ewang1@mit.edu>
2023-09-08 01:01:20 -0400

    deleted period from Jane Street

--- src/lib/sponsors.ts
+++ src/lib/sponsors.ts
@@ -55,7 +55,7 @@ export const SponsorData: Array<Sponsor> = [
     image: JaneStreetImage,
     link: "https://janestreet.com",
     text_md:
-      "Jane Street is a quantitative trading firm that uses innovative technology, a scientific approach and a deep understanding of markets to guide our business. The markets change rapidly, and we need to change faster still. Every day, we come to work with new problems to solve, new systems to build and new theories to test. We’re always looking for people to join us and help come up with that next great idea.",
+      "Jane Street is a quantitative trading firm that uses innovative technology, a scientific approach and a deep understanding of markets to guide our business. The markets change rapidly, and we need to change faster still. Every day, we come to work with new problems to solve, new systems to build and new theories to test. We’re always looking for people to join us and help come up with that next great idea",
   },
   {
     name: "IMC",
